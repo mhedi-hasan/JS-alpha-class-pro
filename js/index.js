f78cbeb88c8ec580f2991305dcc7d52d3551fb02
Mehedi Hasan <mhedih32@gmail.com>
2024-02-15 21:25:05 +0600

    button acces added

--- js/index.js
+++ js/index.js
@@ -10,22 +10,41 @@
 
 function handleKeyboardButtonPress(event) {
   const playerPressed = event.key;
-//   console.log("PlayerPressed:", playerPressed);
+  //   console.log("PlayerPressed:", playerPressed);
 
   // get the expectation to press
   const currentAlphabetElement = document.getElementById("current-alphabet");
   const currentAlphabet = currentAlphabetElement.innerText;
   const expectedAlphabet = currentAlphabet.toLowerCase();
-//   console.log(playerPressed, expectedAlphabet);
+  //   console.log(playerPressed, expectedAlphabet);
 
   //Check to match
   if (playerPressed === expectedAlphabet) {
     console.log("You got a point!");
     console.log("You have pressed correctly:", expectedAlphabet);
+    // Update score
+    // 1: get the current score
+    const currentScoreElement = document.getElementById("current-score");
+    const currentScoreText = currentScoreElement.innerText;
+    const currentScores = parseInt(currentScoreText);
+    console.log(currentScores);
+    // 2: increase the score by 1
+    const newScore = currentScores + 1;
+    // 3: show the Update score
+    currentScoreElement.innerText = newScore;
+    // start a new round
     removeBackgroundColorById(expectedAlphabet);
     continueGame();
   } else {
     console.log("You Missed.You lost a life");
+    //-->1: get the current life number
+    const currentLifeElement = document.getElementById('current-life');
+    const currentLifeText = currentLifeElement.innerText;
+    const currentLife = parseInt(currentLifeText);
+    //-->2: reduce the life count
+    const newLife = currentLife - 1;
+    //-->3: display the update life count
+    currentLifeElement.innerText = newLife;
   }
 }
 document.addEventListener("keyup", handleKeyboardButtonPress);
